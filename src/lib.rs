a58e2a18f8aad9383bab57d39385a9e1bf898910
PORTALSURFER <portalsurfer@gmail.com>
2023-05-20 17:59:57 +0200

    feat: refactor initialization so that texture can be shader

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,19 @@ use wgpu::{CommandBuffer, CommandEncoder, FragmentState, InstanceDescriptor, Ver
 use wgpu::util::DeviceExt;
 
 pub struct Renderer {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    texture: wgpu::Texture,
+    view: wgpu::TextureView,
     is_initialized: bool,
 }
 
 impl Renderer {
-    pub fn new() -> Self {
-        Self {
-            is_initialized: true,
-        }
+    pub fn new_blocking() -> Self {
+        futures::executor::block_on(Self::new())
     }
 
-    pub async fn render_triangle(&self) {
-        let vertices = [
-            [-0.5, -0.5],   // Bottom-left vertex
-            [0.5, -0.5],   // Bottom-right vertex
-            [0.0, 0.5],   // Top vertex
-        ];
-
-        let indices = [0, 1, 2];
-
+    pub async fn new() -> Self {
         let instance = wgpu::Instance::new(InstanceDescriptor {
             backends: wgpu::Backends::PRIMARY,
             dx12_shader_compiler: Default::default(),
@@ -39,16 +33,51 @@ impl Renderer {
             limits: wgpu::Limits::default(),
         }, None).await.unwrap();
 
-        let mut command_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        let texture = device.create_texture(&wgpu::TextureDescriptor {
+            label: None,
+            size: wgpu::Extent3d {
+                width: 800,
+                height: 600,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        });
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        Self {
+            device,
+            queue,
+            texture,
+            view,
+            is_initialized: true,
+        }
+    }
+
+    pub fn render_triangle(&self) {
+        let vertices = [
+            [-0.5, -0.5],   // Bottom-left vertex
+            [0.5, -0.5],   // Bottom-right vertex
+            [0.0, 0.5],   // Top vertex
+        ];
+
+        let indices = [0, 1, 2];
+
+        let mut command_encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
             label: None,
         });
 
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        let shader = self.device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
             source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("../shaders/shader.wgsl"))),
         });
 
-        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        let pipeline = self.device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: None,
             layout: None,
             vertex: VertexState {
@@ -71,28 +100,14 @@ impl Renderer {
             multiview: None,
         });
 
-        let view = device.create_texture(&wgpu::TextureDescriptor {
-            label: None,
-            size: wgpu::Extent3d {
-                width: 800,
-                height: 600,
-                depth_or_array_layers: 1,
-            },
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::RENDER_ATTACHMENT,
-            view_formats: &[],
-        }).create_view(&wgpu::TextureViewDescriptor::default());
 
-        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        let vertex_buffer = self.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: None,
             contents: bytemuck::cast_slice(&vertices),
             usage: wgpu::BufferUsages::VERTEX,
         });
 
-        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        let index_buffer = self.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: None,
             contents: bytemuck::cast_slice(&indices),
             usage: wgpu::BufferUsages::INDEX,
@@ -102,7 +117,7 @@ impl Renderer {
             let mut render_pass = command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: None,
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
+                    view: &self.view,
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
@@ -120,7 +135,7 @@ impl Renderer {
         }
 
         let command_buffer = command_encoder.finish();
-        queue.submit(std::iter::once(command_buffer));
+        self.queue.submit(std::iter::once(command_buffer));
     }
 
     pub fn save_to_texture_on_disk(&self, file_path: &str) {
@@ -145,14 +160,14 @@ mod tests {
 
     #[test]
     fn test_render_initialization() {
-        let renderer = Renderer::new();
+        let renderer = Renderer::new_blocking();
         assert!(renderer.is_initialized(), "Renderer was not initialized");
     }
 
     #[test]
     fn test_render_to_texture_on_disk() {
-        let renderer = Renderer::new();
-        futures::executor::block_on(renderer.render_triangle());
+        let renderer = Renderer::new_blocking();
+        renderer.render_triangle();
         renderer.save_to_texture_on_disk("output.png");
         assert!(std::path::Path::new("output.png").exists(), "Texture was not saved on disk");
     }
